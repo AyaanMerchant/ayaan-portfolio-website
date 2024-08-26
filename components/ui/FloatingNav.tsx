184965972897c37f7c6074a71a5d5c6ee9224a2d
Ayaan Merchant <krmkae786@gmail.com>
2024-08-26 17:03:46 +0500

    edited the Nav bar

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -68,7 +68,7 @@ export const FloatingNav = ({
         style={{
           backdropFilter: "blur(16px) saturate(180%)",
           backgroundColor: "rgba(17, 25, 40, 0.75)",
-          borderRadius: "12px",
+          borderRadius: "24px",
           border: "1px solid rgba(255, 255, 255, 0.125)",
         }}
       >
